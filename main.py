1a09fc6f9608a9cbe539a5f897dcee03a48dbc99
deafSpy <rockingshreyu@gmail.com>
2024-09-01 19:30:40 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -103,7 +103,6 @@ if __name__ == "__main__":
     
     model = LightningModel(config)
     
-    
     shutil.rmtree(os.path.join(config['dataset_path'], config['final_dir_name']), ignore_errors=True)
     os.makedirs(os.path.join(config['dataset_path'], config['final_dir_name']), exist_ok=True)
     
